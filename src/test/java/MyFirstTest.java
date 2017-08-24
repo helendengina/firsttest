d48b7b70bcd2c4e0b07787f5b11923b5b69bba28
Helen Dengina <helen.dengina@yandex.ru>
2017-08-24 10:47:19 +0500

    Добавила описание к коду.

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -3,12 +3,14 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import java.util.concurrent.TimeUnit;
 
 public class MyFirstTest {
     private static WebDriver driver;
 
+    /**
+     * Открывает браузер Google Chrome.
+     */
     @BeforeClass
     public static void setup() {
         System.setProperty("webdriver.chrome.driver", "C:/Users/dengina.elena/Documents/chromedriver.exe");
@@ -16,11 +18,17 @@ public class MyFirstTest {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
+    /**
+     * Открывает поиск Google.
+     */
     @Test
     public void myFirstTest() {
         driver.get("https://google.com");
     }
 
+    /**
+     * Закрывает браузер.
+     */
     @AfterClass
     public static void stop() {
         driver.quit();
